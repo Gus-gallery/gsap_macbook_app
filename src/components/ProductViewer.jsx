0d4aed07a78ca7ad4eed26c04f4d4a9b71e8cc6d
Goose <207717976+Gus-gallery@users.noreply.github.com>
2025-10-25 17:52:28 +0200

    Added light and model

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,6 +3,8 @@ import useMacbookStore from '../store'
 import clsx from 'clsx';
 import { Canvas } from '@react-three/fiber';
 import { Box, OrbitControls } from '@react-three/drei';
+import MacbookModel14 from './models/Macbook-14';
+import StudioLights from './StudioLights';
 
 const ProductViewer = () => {
     const { color, scale, setColor, setScale } = useMacbookStore();
@@ -45,7 +47,9 @@ const ProductViewer = () => {
         </div>
 
         <Canvas className="w-full! h-[80vh]! lg:h-dvh! relative z-40" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
-            <Box position={[0, 0, 0]} scale={10 * scale} material-color={color}></Box>
+            <StudioLights />
+            
+            <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
 
             <OrbitControls enableZoom={false} />
         
